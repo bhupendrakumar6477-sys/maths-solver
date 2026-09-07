bee0bd776a819a91a8a19046236fb432f3a14dde
bhupendrakumar6477-sys <bhupendrakumar6477@gmail.com>
2026-09-07 13:21:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+  document.querySelector('.nav').classList.toggle('open');
+}
+function comingSoon(){
+  alert('यह section अभी तैयार किया जा रहा है। PDF/Test जोड़ने के बाद यह button काम करेगा।');
+}
